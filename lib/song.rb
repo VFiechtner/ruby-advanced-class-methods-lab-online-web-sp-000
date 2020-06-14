dbcc1f8dde2adea8cf5600de9b7a6e7b2da8e43d
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-06-14 22:44:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -61,7 +61,7 @@ class Song
   def self.create_from_filename(filename)
     result = self.new_from_filename(filename)
     song = self.create
-    song_match.name = result.name
+    song.name = result.name
     song.artist_name = result.artist_name
     song
   end
